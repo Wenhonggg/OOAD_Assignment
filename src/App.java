9345e8d94ecf595b8a08902fb40c2e01b730ed52
Wendyyy21 <wendykhong916@gmail.com>
2025-06-24 16:27:39 +0800

    start

---
+++ src/App.java
@@ -0,0 +1,19 @@
+
+import javax.swing.*;
+
+public class App extends JFrame {
+    public App() {
+        super();
+        // add(new PaymentPage(this));
+        add(new MyEventsPage(this));
+        setResizable(false);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // setSize(new Dimension(600,800));
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new App();
+    }
+}
