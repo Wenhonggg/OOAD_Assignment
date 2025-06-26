2f9ef56f3efb1c046225dc9508f4d4228fccad28
Ziyin <angziyin2004@gmail.com>
2025-06-26 13:38:34 +0800

    fixed ugly bat file

--- run.bat
+++ run.bat
@@ -1,11 +1,17 @@
+@REM @echo off
+@REM REM Create bin directory if it doesn't exist
+@REM if not exist "bin" mkdir "bin"
+
+@REM REM Compile all Java files and place class files in bin
+@REM javac -d bin -cp ".;lib\*" src\*.java
+
+@REM REM Run the program from the bin directory
+@REM java -cp "bin;lib\*" LoginApp
+
+@REM pause
+
 @echo off
-REM Create bin directory if it doesn't exist
 if not exist "bin" mkdir "bin"
-
-REM Compile all Java files and place class files in bin
-javac -d bin -cp ".;lib\*" src\*.java
-
-REM Run the program from the bin directory
-java -cp "bin;lib\*" LoginApp
-
+javac --enable-preview -d bin -cp ".;lib\*" src\main\component\*.java src\main\*.java
+java --enable-preview -cp "bin;lib\*" main.LoginPage
 pause
